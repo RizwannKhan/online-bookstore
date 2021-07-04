3acb3255d581eb6c8edec5d468b6a2643c54f363
Rizz <in.rizwan.khan@gmail.com>
2021-07-04 19:45:19 +0530

    Created angular project and completed the feature 1.

--- OnlineBookStore/src/main/java/com/onlinestore/onlinebookstore/repository/BookRepository.java
+++ OnlineBookStore/src/main/java/com/onlinestore/onlinebookstore/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package com.onlinestore.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import com.onlinestore.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200/")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
---
+++ angular-bookstore/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './services/book.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [
+    BookService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
