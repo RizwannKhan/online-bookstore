a12e4ffaccc5c424963ca0c47514c642dbe5b231
Rizz <in.rizwan.khan@gmail.com>
2021-07-06 14:24:40 +0530

    Completed Feature 5 Developement of Angular Application.

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -13,6 +13,8 @@ export class BookListComponent implements OnInit {
 
   books: Book[] = [];
   currentCategoryId?: number;
+  searchMode?: boolean;
+  //keyword?: string;
 
   constructor(private _bookService: BookService, private _activatedRoute: ActivatedRoute) { }
 
@@ -23,6 +25,18 @@ export class BookListComponent implements OnInit {
   }
 
   listBooks() {
+    this.searchMode = this._activatedRoute.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      //do the search work
+      this.handleSearchBooks();
+    } else {
+      //display books based on category
+      this.handleListBooks();
+    }
+  }
+
+  handleListBooks() {
     const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
@@ -35,6 +49,17 @@ export class BookListComponent implements OnInit {
     this._bookService.getBooks(this.currentCategoryId).subscribe(
       data => this.books = data
     )
+  }
+
+  handleSearchBooks() {
+    const keyword: string = this._activatedRoute.snapshot.paramMap.get('keyword')!;
+
+    this._bookService.searchBooks(keyword).subscribe(
+      data => {
+        //console.log(data);  
+        this.books = data;      
+      }
+    );
   }
 
 }
